955913e7a3077fa2aa22199c41cdc9778be96ae1
Ya Yun Huang <yayun@Yas-MacBook-Pro.local>
2022-11-22 15:36:00 -0500

    test-flas-umbuntu

--- application.py
+++ application.py
@@ -15,5 +15,48 @@ def get_items():
     return jsonify(aws_controller.get_items())
 
 
+@app.route("/add-item/<string:id>/")
+def add_item(id):
+    # check if the id already exist
+    response_check = aws_controller.get_itemfrom_id(id)
+    if response_check["ResponseMetadata"]["HTTPStatusCode"] == 200:
+        if "Item" in response_check:
+            return f"This customer already existx", {"Item": response_check["Item"]}
+
+    # If the customer does not exist, add new customer information
+    response = aws_controller.insert_item(id)
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+        return {
+            "msg": "Added successfully",
+        }
+    return {"msg": "Some error occcured", "response": response}
+
+
+@app.route("/read-item/<string:id>")
+def read_item(id):
+    response = aws_controller.get_itemfrom_id(id)
+
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+
+        if "Item" in response:
+            return {"Item": response["Item"]}
+
+        return {"msg": "Customer not found!"}
+
+    return {"msg": "Some error occured", "response": response}
+
+
+@app.route("/delete-item/<string:id>")
+def delete_item(id):
+    response = aws_controller.delete_item(id)
+
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+        return {
+            "msg": "Deleted successfully",
+        }
+
+    return {"msg": "Some error occcured", "response": response}
+
+
 if __name__ == "__main__":  # on running python app.py
     app.run()  # run the flask app
--- aws_controller.py
+++ aws_controller.py
@@ -1,7 +1,66 @@
 import boto3
+import os
+from dotenv import load_dotenv
 
-dynamo_client = boto3.client("dynamodb")
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+REGION_NAME = os.getenv("REGION_NAME")
+
+
+dynamo_client = boto3.client(
+    "dynamodb",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME,
+)
+
+dynamo_resource = boto3.resource(
+    "dynamodb",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME,
+)
+
+CustomerTable = dynamo_resource.Table("customer_table")
 
 
 def get_items():
     return dynamo_client.scan(TableName="customer_table")
+
+
+# first_name, last_name, gender, birthdate, email
+def insert_item(id):  # first_name, last_name, gender, birthdate, mobile, email):
+    response = CustomerTable.put_item(
+        Item={
+            "customer_id": id,
+            # "first_name": first_name,
+            # "last_name": last_name,
+            # "gender": gender,
+            # "mobile": mobile,
+            # "birthdate": birthdate,
+            # "email": email,
+        }
+    )
+    return response
+
+
+def get_itemfrom_id(id):
+    response = CustomerTable.get_item(
+        Key={"customer_id": id},
+        AttributesToGet=[
+            "first_name",
+            "last_name",
+            "gender",
+            "mobile",
+            "birthdate",
+            "email",
+        ],
+    )
+    return response
+
+
+def delete_item(id):
+    response = CustomerTable.delete_item(Key={"customer_id": id})
+    return response
